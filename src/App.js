cdab465c9676fba340edbe134cce73cb0b9a8bdc
Bruna Reveriego <Bruna.Reveriego@br.ey.com>
2022-02-01 19:09:12 -0300

    adicionado css e ajustado o componente app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Component } from "react/cjs/react.production.min";
 import PokeCard from "./components/PokeCard";
+import './components/Pokedex.css';
 
 class App extends Component {
   constructor() {
@@ -9,15 +10,18 @@ class App extends Component {
       pokemons: [],
       pokemonDetails: [],
       pokemonName: null,
-      offset: 0,
+      offset: 1,
       loadNumber: 24,
     };
     this.handleMoreClick = this.handleMoreClick.bind(this);
   }
 
   getNextOffset() {
-    console.log(this.state.offset);
-    return this.state.offset + this.state.loadNumber;
+    return this.state.offset + 1;
+  }
+
+  getPreviousOffset() {
+    return this.state.offset - 1;
   }
 
   handleMoreClick(event) {
@@ -27,34 +31,36 @@ class App extends Component {
     });
   }
 
+  handleMinusClick(event) {
+    if (this.state.offset > 1) {
+      const oldOffset = this.getPreviousOffset();
+
+      this.setState({ offset: oldOffset }, () => {
+        this.getMorePokemon();
+      });
+    }
+  }
+
   componentDidMount() {
     this.getMorePokemon();
   }
 
   getMorePokemon() {
-    let url =
-      "https://pokeapi.co/api/v2/pokemon?offset=" +
-      this.state.offset +
-      "&limit=" +
-      this.state.loadNumber;
+    let url = "https://pokeapi.co/api/v2/pokemon/" + this.state.offset;
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
         if (data) {
-          this.setState({ pokemons: data.results }, () => {
-            this.state.pokemons.map((pokemon) => {
-              fetch(pokemon.url)
-                .then((response) => response.json())
-                .then((data) => {
-                  if (data) {
-                    var temp = this.state.pokemonDetails;
-                    temp.push(data);
-                    this.setState({ pokemonDetails: temp });
-                  }
-                })
-                .catch(console.log);
-            });
-          });
+          if (this.state.pokemonDetails.length === 0) {
+            var temp = this.state.pokemonDetails;
+            temp.push(data);
+            this.setState({ pokemonDetails: temp });
+          } else {
+            temp = this.state.pokemonDetails;
+            temp.shift();
+            temp.push(data);
+            this.setState({ pokemonDetails: temp });
+          }
         }
       })
       .catch(console.log);
@@ -69,15 +75,68 @@ class App extends Component {
 
     return (
       <div className="container">
-        <div className="row">{renderedPokemonList}</div>
+        <div class="row">
+          <div class="col-sm-6 col-md-6 col-lg-6 principal">
+            <div class="principal-conteudo">
+              <div class="circulo">
+                <div class="col circulo-principal">
+                  <div class="col circulo-principal2">
+                    <div class="col circulo-principal3"></div>
+                  </div>
+                </div>
+
+                <div class="col circulo-principal-pequeno">
+                  <div class="circulo-principal-painel vermelho"></div>
+                  <div class="circulo-principal-painel amarelo"></div>
+                  <div class="circulo-principal-painel verde"></div>
+                </div>
+              </div>
+              <div class="linha-principal">
+                <div class="linha-principal-reta"></div>
+                <div class="linha-principal-inclinada"></div>
+                <div class="linha-principal-reta2"></div>
+              </div>
+
+              <div class="principal-baixo">
+                <div class="principal-baixo-borda-superior"></div>
+                <div class="principal-baixo-borda-superior-inclinada"></div>
+                <div class="principal-baixo-borda-superior-reta"></div>
+
+                <div class="principal-baixo-borda-superior-lateral"></div>
+
+                <div class="linha-principal-lateral">
+                  <div class="linha-principal-lateral-reta"></div>
+                </div>
+
+                <div class="col principal-visor">
+                  <div class="col principal-visor2"></div>
+                  <div>{renderedPokemonList}</div>
+                  <div class="col principal-visor-corte"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-3 col-md-6 col-lg-6 principal"></div>
+        </div>
+
+   
+        <button
+          type="button"
+          className="btn btn-secondary btn-block"
+          key="minus-button"
+          id="minus-button"
+          onClick={this.handleMinusClick.bind(this)}
+        >
+          Previous
+        </button>
         <button
           type="button"
           className="btn btn-secondary btn-block"
           key="more-button"
           id="more-button"
-          onClick={this.handleMoreClick}
+          onClick={this.handleMoreClick.bind(this)}
         >
-          Load More
+          Next
         </button>
       </div>
     );
